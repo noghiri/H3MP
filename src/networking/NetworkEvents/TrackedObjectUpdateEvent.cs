f2bf5313d68d12c2befe4eb81cac01e158d5cf4b
noghiri <noghiri@gmail.com>
2023-10-28 12:44:45 -0700

    Missed a file

---
+++ src/networking/NetworkEvents/TrackedObjectUpdateEvent.cs
@@ -0,0 +1,23 @@
+﻿using H3MP.src.networking.clientEventReg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace H3MP.src.networking.NetworkEvents
+{
+    class TrackedObjectUpdateEvent : NetworkEvent
+    {
+
+        public override void Serialize()
+        {
+            // Code here to actually write stuff into a byte array for transmission
+
+        }
+
+        public override void Deserialize()
+        {
+            // Code here to actually read stuff from a byte array from transmission 
+        }
+    }
+}
